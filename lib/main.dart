b2d469a34e38a6d231ce63704689d38f0a93569a
Miracle <nyinyizin1818@gmail.com>
2025-08-27 13:40:00 +0630

    56. Challenge Solution 2/2 - Working with More Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,23 @@ import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 79, 18, 185),
+                Color.fromARGB(255, 106, 43, 215),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
